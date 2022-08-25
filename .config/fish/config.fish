e8e112de3e7578cc1e3e8f0cbd0d2e87b4b11cf0
Vazanoir <vazanoir@outlook.fr>
2022-08-25 20:20:58 +0200

    Add fish config file

---
+++ .config/fish/config.fish
@@ -0,0 +1,56 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+cat $HOME/roadmap
+echo
+
+# starship
+starship init fish | source
+
+# general
+alias config="git --git-dir=$HOME/.cfg/ --work-tree=$HOME"                  
+set SSD "/mnt/samsung-ssd-840"
+set HDD "/mnt/hdd"
+alias v="nvim"
+alias c="code"
+
+# fish
+set FISH_CONFIG "$HOME/.config/fish/config.fish"
+
+# bin
+fish_add_path "$HOME/bin"
+
+# wine
+alias wine-ge "$HOME/wine/lutris-GE-Proton7-25-x86_64/bin/wine"
+
+# neovim
+set ASTROVIM_CONFIG "$HOME/.config/nvim/lua/user/init.lua"
+
+# fzf
+set fzf_fd_opts --hidden --exclude=.git
+
+# node
+alias setDefaultNodeVersion="set --universal nvm_default_version"
+
+# pnpm
+set -gx PNPM_HOME "$HOME/.local/share/pnpm"
+set -gx PATH "$PNPM_HOME" $PATH
+
+# bun
+set -Ux BUN_INSTALL "$HOME/.bun"
+fish_add_path "$HOME/.bun/bin"
+
+# cargo
+set PATH $HOME/.cargo/bin $PATH
+
+# steam
+set STEAM_COMMON_OS "$HOME/.steam/steam/steamapps/common"
+set -l steamLibraryCommon "SteamLibrary/steamapps/common"
+set STEAM_COMMON_SSD "$SSD/$steamLibraryCommon"
+set STEAM_COMMON_HDD "$HDD/$steamLibraryCommon"
+
+# doom eternal
+set DOOM_ETERNAL "$STEAM_COMMON_OS/DOOMEternal"
+set -l eternalInjector "EternalModInjectorShell.sh"
+alias injectEternal="cd $DOOM_ETERNAL && bash $eternalInjector && cd -"
