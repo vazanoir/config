752fc3e3a576f9d01d7d4b5c1b1a008ef7856f5c
Vazanoir <vazanoir@outlook.fr>
2022-09-07 17:41:58 +0200

    Update 2022-09-07 17:41 Linux/x86_64

--- .astronvim/init.lua
+++ .astronvim/init.lua
@@ -1,1 +1,20 @@
-.config/nvim/lua/user/init.lua
+local config = {
+  mappings = {
+    v = {
+      ["J"] = { ":m '>+1<CR>gv=gv" },
+      ["K"] = { ":m '<-2<CR>gv=gv" }
+    }
+  },
+  plugins = {
+    init = {
+      { "editorconfig/editorconfig-vim" },
+      ["akinsho/bufferline.nvim"] = { disable = true },
+      ["akinsho/toggleterm.nvim"] = { disable = true },
+    },
+    ["show-key"] = {
+      show_help = false,
+    }
+  }
+}
+
+return config;
