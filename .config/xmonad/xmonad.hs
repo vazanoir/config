3255ddc181806d2b10d7ad0dbb5bd2791832a90d
Vazanoir <vazanoir@outlook.fr>
2022-09-19 01:45:36 +0000

    Update 2022-09-19 01:45 Linux/x86_64

--- .config/xmonad/xmonad.hs
+++ .config/xmonad/xmonad.hs
@@ -1,8 +1,16 @@
 import XMonad
+
 import XMonad.Config.Desktop
+import XMonad.Config.Bepo
+
+import XMonad.Hooks.WindowSwallowing
+
+import qualified Data.Map as M
 
 main :: IO ()
 main = xmonad desktopConfig
-	{ terminal 		= "alacritty"
-	, borderWidth = 3
+  { keys            = \c -> bepoKeys c `M.union` keys desktopConfig c
+	, terminal        = "alacritty"
+	, borderWidth     = 3
+	, handleEventHook = swallowEventHook (className =? "Alacritty") (return True)
 	}
