f47838e9776100bcab51488ca29e20e080892a0f
Vazanoir <vazanoir@outlook.fr>
2022-10-19 20:01:44 +0200

    Update 2022-10-19 20:01 Linux/x86_64

--- .config/xmobar/xmobar.hs
+++ .config/xmobar/xmobar.hs
@@ -5,7 +5,7 @@ Config { font = "xft:Source Sans:size=10:semibold:antialias=true"
        , bgColor = "#1c2023"
        , fgColor = "#c7ccd1"
        , position = TopH 24
-       , commands = [ Run Weather "LFLY" ["-t","<station>: <tempC>°C","-L","18","-H","25","--normal","green","--high","red","--low","lightblue"] 36000
+       , commands = [ Run Weather "LFLY" ["-t","<station>: <tempC>°C","-L","13","-H","25","--normal","#95C7AE","--high","#C7AE95","--low","#95AEC7"] 36000
                     , Run Cpu ["-t", "CPU <total>%"] 10
     		            , Run DateZone "%H:%M:%S - %a %_d %b %Y" "fr_FR.UTF-8" "Europe/Paris" "date" 10
     		            , Run DynNetwork ["-t","<fn=1>\xf6d9</fn> <rx> - <fn=1>\xfa51</fn> <tx>", "-S", "true"] 10
