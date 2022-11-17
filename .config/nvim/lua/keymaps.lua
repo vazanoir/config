a20c40e2ed3b726124ef44c2650d4f2955e6e398
Vazanoir <vazanoir@outlook.fr>
2022-11-17 16:54:06 +0100

    Update keymaps.lua

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -2,19 +2,25 @@ local function keymap(m, k, v)
 	vim.keymap.set(m, k, v, { silent = true })
 end
 
+-- Telescope
 keymap('n', '<leader>ff', ":Telescope find_files<cr>")
 keymap('n', '<leader>fF', ":Telescope find_files find_command=rg,--ignore,--hidden,--files<cr>")
 keymap('n', '<leader>fg', ":Telescope live_grep<cr>")
 keymap('n', '<leader>fb', ":Telescope buffers<cr>")
 keymap('n', '<leader>fh', ":Telescope help_tags<cr>")
 
+-- Packer
 keymap('n', '<leader>pi', "<cmd>PackerInstall<cr>")
 keymap('n', '<leader>pu', "<cmd>PackerUpdate<cr>")
 keymap('n', '<leader>pc', "<cmd>PackerClear<cr>")
 keymap('n', '<leader>ps', "<cmd>PackerSync<cr>")
 
-keymap('n', '<leader>lr', "<cmd>lua vim.lsp.buf.rename()<cr>")
+-- Lsp
+-- keymap('n', '<leader>lr', "<cmd>lua vim.lsp.buf.rename()<cr>")
 keymap('n', '<leader>ld', "<cmd>lua vim.diagnostic.open_float()<cr>")
+keymap('n', '=', "<cmd>LspZeroFormat<cr>")
+keymap('v', '=', ":'<,'>LspZeroFormat<cr>")
 
-keymap('v', 'J', ":m '>+1<CR>gv=gv")
-keymap('v', 'K', ":m '<-2<CR>gv=gv")
+-- Other
+keymap('v', 'J', ":m '>+1<CR>gv:'<,'>LspZeroFormat<cr>gv")
+keymap('v', 'K', ":m '<-2<CR>gv:'<,'>LspZeroFormat<cr>gv")
